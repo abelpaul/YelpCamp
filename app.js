cded5cb6f6decd6f12e4bbb5dac9268ed2c089a2
Abel George <abel.paul.george@gmail.com>
2018-11-12 15:40:34 -0500

    added post route

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+let campgrounds = [
+    {name: "Salmon Creek", image: "http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg"},
+    {name: "Stone Mountain", image: "https://acadiamagic.com/280x187/md-campground.jpg"},
+    {name: "Yee Haw Forest", image: "https://www.reserveamerica.com/webphotos/NH/pid270015/0/540x360.jpg"}
+]
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/",function(req,res){
@@ -9,12 +17,23 @@ app.get("/",function(req,res){
 
 
 app.get("/campgrounds",function(req,res){
-    let campgrounds = [
-        {name: "Salmon Creek", image: "http://www.suttonfalls.com/communities/4/004/012/498/244//images/4628314067.jpg"},
-        {name: "Stone Mountain", image: "https://acadiamagic.com/280x187/md-campground.jpg"},
-        {name: "Yee Haw Forest", image: "https://www.reserveamerica.com/webphotos/NH/pid270015/0/540x360.jpg"}
-    ]
+    
     res.render("campgrounds", {campgrounds:campgrounds})
+})
+
+app.post("/campgrounds", function(req,res){
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = {
+        name: name,
+        image:image
+    }
+campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+})
+
+app.get("/campgrounds/new", function(req,res) {
+res.render("new")
 })
 
 app.listen(3000,function(){
